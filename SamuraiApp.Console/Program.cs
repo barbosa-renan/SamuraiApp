1b569805083631a7ae24f113b1e18fedfddea1ac
Renan Barbosa <renan.q.barbosa@avanade.com>
2021-05-17 18:23:19 -0300

    Inserindo e obtendo dados

---
+++ SamuraiApp.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SamuraiApp.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/SamuraiApp.UI/Program.cs
@@ -0,0 +1,42 @@
+﻿using SamuraiApp.Data;
+using SamuraiApp.Domain;
+using System;
+using System.Linq;
+
+namespace SamuraiApp.UI
+{
+    class Program
+    {
+        private static SamuraiContext _context = new SamuraiContext();
+
+        public static void Main(string[] args)
+        {
+            _context.Database.EnsureCreated();
+
+            GetSamurais("Before Add");
+
+            AddSamurai();
+
+            GetSamurais("After Add");
+
+            Console.Write("Press any key:");
+            Console.ReadKey();
+        }
+
+        private static void GetSamurais(string v)
+        {
+            var samurais = _context.Samurais.ToList();
+            Console.WriteLine($"{v}: Samurai count is {samurais.Count}");
+
+            foreach (var samurai in samurais)
+                Console.WriteLine(samurai.Name);
+        }
+
+        private static void AddSamurai()
+        {
+            var samurai = new Samurai { Name = "Renan" };
+            _context.Samurais.Add(samurai);
+            _context.SaveChanges();
+        }
+    }
+}
